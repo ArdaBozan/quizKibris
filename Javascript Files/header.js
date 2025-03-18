9fb8d7c9e44a16e05870d4366b44b8590ad39d68
Arda Bozan <bozanarda9@gmail.com>
2025-03-18 08:47:29 +0300

    h

--- Javascript Files/header.js
+++ Javascript Files/header.js
@@ -4,3 +4,14 @@ const otherLanguages = document.querySelector('header .otherLanguages');
 languageBtn.addEventListener('click', function() {
     otherLanguages.classList.toggle('show');
 });
+
+
+const nav = document.querySelector('header nav');
+const navList = document.querySelector('header nav ul');
+const hamburgerBtn = document.querySelector('header .hamburgerBtn');
+
+hamburgerBtn.addEventListener('click', function() {
+    nav.classList.toggle('show');
+    navList.classList.toggle('show');
+    hamburgerBtn.classList.toggle('active');
+});
